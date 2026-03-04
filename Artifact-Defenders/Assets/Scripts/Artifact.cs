50ac6a4c093851ff2ac16c31fde9513647662473
ductrong1710 <ductrong.g73@gmail.com>
2026-03-05 05:48:57 +0700

    update asset

--- Artifact-Defenders/Assets/Scripts/Artifact.cs
+++ Artifact-Defenders/Assets/Scripts/Artifact.cs
@@ -4,61 +4,7 @@ using UnityEngine;
 
 /// <summary>
 /// Holds and manages the information about the Artifact's health and provides functions to damage and restore it's health.
-/// </summary>public class Artifact : MonoBehaviour
-//{
-//    public int health;
-//public int maxHealth;
-
-//public int bleed;
-//AudioSource audioSource;
-//float timer;
-
-//void Awake()
-//{
-//    audioSource = GetComponent<AudioSource>();
-//    health = maxHealth;
-//    timer = Time.time + 1;
-//}
-
-//void Update()
-//{
-//    if (Time.time > timer)
-//    {
-//        health -= bleed;
-//        timer = Time.time + 1;
-//    }
-
-//    if (health <= 0)
-//    {
-//        health = 0;
-//    }
-//}
-//public void Damage(int amount)
-//{
-//    health -= amount;
-//}
-//public void SetMaxHealth(int newMax)
-//{
-//    maxHealth = newMax;
-//    health = maxHealth;
-//    Debug.Log($"💪 Trụ được tăng máu tối đa lên {maxHealth}");
-//}
-//private void OnTriggerEnter2D(Collider2D collision)
-//{
-//    if (collision.GetComponent<PlayerBackpack>() != null)
-//    {
-//        if (collision.GetComponent<PlayerBackpack>().current != 0)
-//        {
-//            audioSource.Play();
-//        }
-//        health += collision.GetComponent<PlayerBackpack>().TakeFruits();
-//        if (health > maxHealth)
-//        {
-//            health = maxHealth;
-//        }
-//    }
-//}
-//}
+/// </summary>
 public class Artifact : MonoBehaviour
 {
     public int health;
